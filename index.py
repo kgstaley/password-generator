a8fd16da2a5588b641819d9e378a19b78adeaac2
Kerry Staley <kstaley7819@gmail.com>
2022-07-28 01:29:55 -0700

    better passwords

--- index.py
+++ index.py
@@ -1,5 +1,15 @@
 import string
 from random import*
-characters = string.ascii_letters + string.punctuation + string.digits
-password = "".join(choice(characters) for x in range(randint(8, 16)))
+
+characters = string.ascii_letters + string.digits
+password = ""
+i = 0
+
+while i < 3:
+    if i > 0:
+        password += "-"
+    strHash = "".join(choice(characters) for x in range(5))
+    password += strHash
+    i += 1
+
 print(password)
